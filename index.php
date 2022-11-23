1854efee850cd258beaff4e6312bbae6f42f4c9f
leinad_nofiled <102634472+DanielPCV2020@users.noreply.github.com>
2022-11-23 11:31:34 -0400

    projetin

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Usuários</title>
+</head>
+
+<body>
+    <style>
+      *{
+        margin: 0px;
+      }
+      
+    </style>
+    <?php include_once '_parts/_header.php'; ?>
+    
+    <img style="border: 2px;
+    border-radius: 25px; width: 97.5%; margin-right: 30pxs; margin: 30px; box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;" src="./images/2.jpg">
+   
+    
+    </div>
+    <?php include '_parts/_linkJS.php'; ?>
+</body>
+
+</html>
